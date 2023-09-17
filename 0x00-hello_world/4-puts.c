76618abf822e67b0f7b933daea8fcafddc5aaea8
pauliniso <pauliniso@gmail.com>
2023-09-17 14:34:42 -0700

    A C program that prints exactly statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- * main: a C program that prints with puts function
- *
- * return: ending with the value 0
- */
+* main:
+* a C program that prints with puts function
+*
+* return: ending with the value 0
+*/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return 0;
+	return(0);
 }
+
