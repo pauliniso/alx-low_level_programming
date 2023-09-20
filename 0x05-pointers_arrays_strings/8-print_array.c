dfc82415083f5ad039af34abe9a48de02c369b9d
pauliniso <pauliniso@gmail.com>
2023-09-20 02:39:59 -0700

    a function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -16,7 +15,8 @@ void print_array(int *a, int n)
 		printf("%d, ", a[i]);
 		if (i == (n - 1)
 		{
-		  printf("%d", a[n-1]);
+		  printf("%d", a[n - 1]);
 		}
-	printf("\n");
+
+		printf("\n");
 }
