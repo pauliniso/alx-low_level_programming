21b0d509153639413f31b522f5e84631bf25efd8
pauliniso <pauliniso@gmail.com>
2023-09-21 21:22:52 -0700

    a function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,26 +3,25 @@
 /**
  * leet - function that encodes a string
  * @s: string of characters
- *
  * Return: the result
+ *
  */
 char *leet(char *s)
 {
 	int y, x;
 
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
+	char p[] = "aAeEoOtTlL";
+	char n[] = "4433007711";
 
 	for (y = 0; s[y] != '\0'; y++)
 	{
 		for (x = 0; x < 10; x++)
 		{
-		 	if (s[y] == a[x])
+			if (s[y] == p[x])
 			{
-				s[y] = b[x];
-		  	}
+				s[y] = n[x];
+			}
 		}
 	}
-
 	return (s);
 }
