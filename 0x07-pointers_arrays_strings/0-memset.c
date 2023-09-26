b01aa4c4f566c9b27d77d39d517d6c779785f3ed
pauliniso <pauliniso@gmail.com>
2023-09-25 19:44:09 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory location to be fill
+ * @b: character to be copy
+ * @n: number of times
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
